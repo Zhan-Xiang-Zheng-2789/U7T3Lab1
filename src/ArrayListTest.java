63512b5109306caac4d4d33f436dbb4f75a44dc8
BT_1E10_14 <sample@bths.edu>
2022-02-08 09:58:50 -0500

    Inital Commit

---
+++ src/ArrayListTest.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class ArrayListTest
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Jamal");
+        names.add("Emily");
+        names.add("Destiny");
+        names.add("Mateo");
+
+        //COMPLETE ME: print each name using an index based for loop
+        for (
+        {
+
+        }
+        //COMPLETE ME: print each name (again!) using enhanced for loop
+        for (int name : names)
+        {
+
+        }
+        //COMPLETE ME: print each name (a third time!) using a while loop
+        int i =
+        while (
+                {
+
+                }
+    }
+}
