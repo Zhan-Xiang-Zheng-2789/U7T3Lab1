093ce8773716d0acb9058f4d189f6b3585b5f68d
Bob <zhanzheng@Z-MacBook-Pro.local>
2022-02-08 21:46:04 -0500

    Inital Commit

---
+++ src/Digits.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Digits
+{
+    /**
+     * The list of digits from the number used to construct this object.
+     * The digits appear in the list in the same order in which they appear in the original number.
+     */
+    private ArrayList<Integer> digitList;
+
+    /**
+     * Constructs a Digits object that represents num.
+     * PRECONDITION: num >= 0
+     */
+    public Digits(int num)
+    {
+        int currentNum = num;
+        while (currentNum != 0);
+        {
+            digitList.add(0, currentNum % 10);
+            currentNum = currentNum / 10;
+        }
+    }
+
+    // GETTER METHOD: added to enable test code (not provided in original FRQ)
+    public ArrayList<Integer> getDigitList()
+    {
+        return digitList;
+    }
+
+    /**
+     * Returns true if the digits in this Digits object are in strictly increasing order;
+     * false otherwise.
+     */
+    public boolean isStrictlyIncreasing()
+    {
+        int pastInt = digitList.get(0);
+        for (int i = 1; i < digitList.size(); i++)
+        {
+            if (digitList.get(i) < pastInt)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
